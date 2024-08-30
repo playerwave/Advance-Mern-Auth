e01a1391a84436b1fbbe68b608e34eb5c9aa1127
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-08-30 23:37:02 +0700

    database setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World ");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(3001, () => {
+  connectDB();
+  console.log("Server is running on port ", PORT);
+});
---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+},{timestamps: true});
+
+//create and update at field will be automatically added into the document
+
+export const User = mongoose.model('User', userSchema);
