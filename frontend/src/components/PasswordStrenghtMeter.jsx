96a251b320b9ca9de81388f92f8cbf497740336e
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-09-03 01:29:05 +0700

    Signup Page UI (fixed bugs)

--- frontend/src/components/PasswordStrenghtMeter.jsx
+++ frontend/src/components/PasswordStrenghtMeter.jsx
@@ -2,9 +2,9 @@ import { Check, X } from "lucide-react";
 
 const PasswordCriteria = ({ password }) => {
   const criteria = [
-    { label: "At least 6 characters", met: password.lenght >= 6 },
-    { label: "Contains uppercase letter", met: /A-Z/.test(password) },
-    { label: "Contains lowercase letter", met: /A-Z/.test(password) },
+    { label: "At least 6 characters", met: password.length >= 6 },
+    { label: "Contains uppercase letter", met: /[A-Z]/.test(password) },
+    { label: "Contains lowercase letter", met: /[a-z]/.test(password) },
     { label: "Contains a number", met: /\d/.test(password) },
     { label: "Contains special character", met: /[^A-Za-z0-9]/.test(password) },
   ];
