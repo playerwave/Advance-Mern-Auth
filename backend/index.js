6cb8f943f9117d537bc120ea4b5585a269c4b624
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-09-03 23:59:47 +0700

    implementing signup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -15,6 +16,8 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello World ");
 });
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); //allows us to parse incoming requests:req.body
 app.use(cookieParser()); // alows us to parse incoming cookies
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,17 +1,27 @@
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Mail, User, Lock } from "lucide-react";
+import { Mail, User, Lock, Loader } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrenghtMeter from "../components/PasswordStrenghtMeter";
+import { useAuthStore } from "../store/authStore";
 
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { signup, error, isLoading } = useAuthStore();
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    try {
+      await signup(email, password, name);
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -53,6 +63,7 @@ const SignUpPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
           <PasswordStrenghtMeter password={password} />
 
           <motion.button
@@ -63,8 +74,13 @@ const SignUpPage = () => {
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
             type="submit"
+            disabled={isLoading}
           >
-            Sign Up
+            {isLoading ? (
+              <Loader className="animate-spin mx-auto" size={24} />
+            ) : (
+              "Sign Up"
+            )}
           </motion.button>
         </form>
       </div>
