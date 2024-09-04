f006727a6e0a6c43578b2c5566bb2b97dac786fd
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-09-04 22:59:31 +0700

    fix bug in dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useAuthStore } from "../store/authStore";
-import { formatDate } from "../util/Date";
+import { formatDate } from "../util/date";
 
 const DashboardPage = () => {
   const { user, logout } = useAuthStore();
@@ -71,7 +71,7 @@ const DashboardPage = () => {
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          onClick={handleLogout} 
+          onClick={handleLogout}
           className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg *:
         shadow-lg hover:from-green-600 hover:to-emerald-700
         focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
